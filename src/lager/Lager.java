d506371e2fd83023b0e9b3cbe830a8a9f616a6e8
kmatil <kmatil@PC5897.nordic.local>
2018-08-23 09:20:33 +0200

    first

---
+++ src/lager/Lager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lager;
+
+/**
+ *
+ * @author kmatil
+ */
+public class Lager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        smallman
+        // TODO code application logic here
+    }
+    
+}
